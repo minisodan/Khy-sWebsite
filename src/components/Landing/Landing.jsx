fa41cbdad200015c722ca12f1b530b7b43132aae
BowlBird <bowlbirdcontact@gmail.com>
2024-10-19 22:30:01 -0500

    removed digital

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -5,7 +5,7 @@ const Landing = () => {
     <div class="flex flex-col h-screen bg-cover bg-center bg-landing-image">
       <div class="m-auto animate-fade-in-up flex gap-4 flex-col">
         <h1 class="text-6xl p-1 font-semibold uppercase text-center bg-blue-200 text-purple-950">
-          Khyron Digital
+          Khyron
         </h1>
         <div class="flex justify-center gap-2 bg-blue-200 p-2">
           <Tab text="home" link="/" />
@@ -15,9 +15,7 @@ const Landing = () => {
       </div>
       <div>
         <footer class="flex justify-center bg-blue-200 text-purple-950 mt-auto p-2">
-          <span class="text-sm text-center">
-            © {currentYear} Khyron Digital
-          </span>
+          <span class="text-sm text-center">© {currentYear} Khyron</span>
         </footer>
       </div>
     </div>
---
+++ src/components/MasonryGrid/MasonryGrid.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import Masonry from "react-masonry-css";
+import MasonryEntry from "./MasonryEntry.jsx";
+import "./MasonryGrid.css";
+import useReachedBottom from "../../Hooks/useReachedBottom.jsx";
+import axios from "axios";
+import Navbar from "../Navbar/Navbar.jsx";
+function MasonryGrid({ scrollValue }) {
+  const breakpointColumnsObj = {
+    default: 5,
+    1100: 2,
+    700: 1,
+  };
+
+  const reachBottom = useReachedBottom();
+
+  useEffect(() => {
+    if (reachBottom && lastIndex <= images.length - 1) {
+      let imagesSlice = [...images].slice(0, lastIndex + 20);
+      console.log("imagesliced array:", imagesSlice);
+      setCurrentImages(imagesSlice);
+      setLastIndex(lastIndex + 20);
+    }
+  }, [reachBottom]);
+
+  const [images, setImages] = useState([]);
+  const [currentImages, setCurrentImages] = useState([]);
+  const [lastIndex, setLastIndex] = useState(0);
+
+  return (
+    <Masonry
+      breakpointCols={breakpointColumnsObj}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column"
+    >
+      {currentImages}
+      <div>
+        <small> © 2022 Khyron Site by Mason Myles</small>
+      </div>
+    </Masonry>
+  );
+}
+
+export default MasonryGrid;
