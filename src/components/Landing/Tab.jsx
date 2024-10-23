87027d5d4e76fed2a3419b4dd23279bdec050d24
Mason Myles <59033177+minisodan@users.noreply.github.com>
2024-10-22 22:51:05 -0500

    changing background

--- src/components/Landing/Tab.jsx
+++ src/components/Landing/Tab.jsx
@@ -1,6 +1,6 @@
 const Tab = ({ text, link }) => (
   <a
-    class="uppercase font-semibold text-purple-950 relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-full after:scale-x-0 after:bg-purple-950 after:transition-transform after:duration-300 hover:after:scale-x-100"
+    class="uppercase font-semibold bg-zinc-50 text-slate-800 relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-full after:scale-x-0 after:bg-purple-950 after:transition-transform after:duration-300 hover:after:scale-x-100"
     href={link}
   >
     {text}
