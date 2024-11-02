3f3edbebc9177a81cf3e798fa0178fca1d0a4cac
Mason Myles <mason.myles98@gmail.com>
2024-11-02 16:40:25 -0500

    adding stuff and things

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,25 +3,46 @@ import { Link } from "react-router-dom";
 
 function About() {
   return (
-    <div className="flex flex-col md:flex-row bg-about-image bg-no-repeat bg-cover bg-center">
+    <div className="flex flex-row bg-about-image bg-no-repeat bg-cover bg-center">
       <NavBar />
-      <div className="flex flex-col md:flex-row w-full animate-fade-in-up justify-center items-start p-6 gap-4">
-        <div className="mt-auto mb-auto w-full md:w-1/3 bg-white p-6 mr-0 md:mr-4 flex flex-col justify-between">
+      <div className="flex flex-col w-full md:w-3/4 animate-fade-in-up justify-center items-center p-6 gap-4">
+        {/* Image section */}
+        <div className="w-full lg:w-2/3 mb-4">
+          <img
+            src={"Images/khy20241.jpg"}
+            alt="KHYRON.III"
+            className="h-auto w-full object-cover"
+          />
+        </div>
+        {/* Text section */}
+        <div className="w-full lg:w-2/3 bg-white p-6 flex flex-col justify-between">
           <div>
             <h2 className="text-xl md:text-2xl font-bold text-gray-800">Bio</h2>
             <p className="text-gray-700 text-justify">
-              Born in Killeen TX but raised in Northern Louisiana and now
-              currently attending MNSU (Minnesota State University Mankato) for
-              an Art BA Degree, Khyron's work is a reflection of his community,
-              race, and upbringing. In May 2021, he bought his first DSLR Camera
-              [Sony A7iii] and consistently uploads his photos on this website
-              or his Instagram account [@khyron.iii]. Khy has an eye for
-              capturing real in-the-moment pictures and strives to make the
-              viewer's semblance connected to his work. He's had solo
-              exhibitions in the main art gallery of MNSU titled “Human Xenon”
-              in 2022 and another one downtown, Mankato, in an art space titled
-              the “410 Project,” where he displayed his work titled “Fornever”
-              within the same year.
+              Born in Killeen Texas 1999, but Louisiana raised, and is currently
+              stationed in St. Paul, Minnesota. Khyron has Bachelor's BA in Art
+              (MNSU) with a photography concentration. Khy has been doing
+              photography since May 2021, He bought his first DSLR Camera [Sony
+              A7iii] with inheritance funds from his late mother and
+              consistently uploads his photos on this website or his Instagram
+              account [@khyron.iii] Khy has an eye for capturing real "In the
+              moment" pictures and strives to make the viewer feel like they are
+              fully submerged in the photo; as if they've time traveled to the
+              2D plain themselves. Most of his time is spent doing street
+              photography, but really will shoot anything on his camera from
+              nature to architecture to even studio photographs. Growing up in
+              the south, he was always surrounded by things that seemed to never
+              change, but subconsciously always surrounded by art that seemed to
+              blossom from the heart and soul. He left the south to search for
+              himself and boost his creativity by traveling to different states
+              and countries to raise his creativity ceiling. Khy's interest in
+              anime, movies, comics, etc of all kinds help form his direction
+              when it comes to photography. He wants people not only to take
+              these photos and reminisce with him, but he also wants people to
+              be inspired and let the photos engulf them with memories or
+              experiences of their own; whether it be from the past, future or
+              present. He says "I want to show people memories they never knew
+              they had"
             </p>
           </div>
           <Link to="/contact">
@@ -29,13 +50,6 @@ function About() {
               Get in Touch with Me
             </button>
           </Link>
-        </div>
-        <div className="mt-auto mb-auto w-full lg:w-1/3 lg:h-1/2 md:w-1/3 md:h-auto">
-          <img
-            src={"Images/KHYRON35.jpeg"}
-            alt="KHYRON.III"
-            className="h-auto w-full object-cover md:h-full"
-          />
         </div>
       </div>
     </div>
